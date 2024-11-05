cb1e65a335451462eadb23cd84ecdb332ec6032b
Taya Hanchar <tayahanchar@gmail.com>
2024-11-05 18:06:03 +0200

    feat: add marvel service and get a random character in the random character section. add spinner if we dont't have data

--- src/components/RandomCharacter.jsx
+++ src/components/RandomCharacter.jsx
@@ -1,30 +1,70 @@
 import './RandomCharacter.css';
+import MarvelService from '../marvelService';
+import { Component } from 'react';
 
-function RandomCharacter() {
-  return (
-    <div className="random-character_container">
-      <div className='random-character'>
-        <img src="../../public/Thumbnail.jpg" alt="character" />
-        <div className='random-character_descr'>
-          <h2 className='random-character_title'>THOR</h2>
-          <p className='random-character_p'>As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he is quite smart and compassionate...</p>
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+class RandomCharacter extends Component {
+
+  constructor() {
+    super();
+    this.setCharacterData();
+
+    this.state = {
+      character: {}
+    }
+  }
+
+  newMarvelService = new MarvelService();
+
+  setCharacterData = () => {
+    const randomCharacterId = getRandomIntInclusive(1011400, 1011000);
+
+    this.newMarvelService.getOneCharacterByID(randomCharacterId).then(res => {
+      this.setState({character: res})
+    });
+
+    // this.newMarvelService.getAllCharacters().then(res => console.log(res))
+  }
+
+
+  render() {
+    return (
+      <div className="random-character_container">
+        <div className='random-character'>
+          {
+            this.state.character.name ? 
+            <>
+              <img className='random-character_img' src={this.state.character.img} alt="character" />
+              <div className='random-character_descr'>
+              <h2 className='random-character_title'>{this.state.character.name}</h2>
+              <p className='random-character_p'>{this.state.character.description}</p>
+                <button className='random-character_button'><a href={this.state.character.url} target='_blank'>HOMEPAGE</a></button>
+              </div> 
+            </>: 
+            <div className='loader-container'>
+              <span className="loader"></span>
+            </div>
+          }
+          
+        </div>
+  
+        <div className='random-character_inf'>
           <div>
-            <button className='random-character_button'>HOMEPAGE</button>
-            <button className='random-character_button'>WIKI</button>
+            <p style={{textAlign: 'left'}}>Random character for today!</p>
+            <p>Do you want to get to know him better?</p>
           </div>
+          <p>Or choose another one</p>
+          <button onClick={this.setCharacterData} className='random-character_button'>TRY IT</button>
         </div>
       </div>
-
-      <div className='random-character_inf'>
-        <div>
-          <p style={{textAlign: 'left'}}>Random character for today!</p>
-          <p>Do you want to get to know him better?</p>
-        </div>
-        <p>Or choose another one</p>
-        <button className='random-character_button'>TRY IT</button>
-      </div>
-    </div>
-  )
+    )
+  }
+  
 }
 
 export default RandomCharacter;
---
+++ src/marvelService.js
@@ -0,0 +1,35 @@
+class MarvelService {
+  _apiKey = "apikey=75522c928a8326c06409ab4c867fbb91";
+
+  getResourse = async (url) => {
+    const date = await fetch(url);
+    return await date.json();
+  };
+
+  getAllCharacters = async () => {
+    const result = await this.getResourse(
+      `https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=600&${this._apiKey}`
+    );
+
+    return result.data.results.map((item) => this.setCharacterData(item));
+  };
+
+  getOneCharacterByID = async (id) => {
+    const result = await this.getResourse(
+      `https://gateway.marvel.com:443/v1/public/characters/${id}?${this._apiKey}`
+    );
+
+    return this.setCharacterData(result.data.results[0]);
+  };
+
+  setCharacterData = (result) => {
+    return {
+      name: result.name,
+      description: result.description,
+      img: `${result.thumbnail.path}.${result.thumbnail.extension}`,
+      url: result.urls[0].url,
+    };
+  };
+}
+
+export default MarvelService;
